ea7fefb711edaf9d822db97ed76fff9a5f238044
cho881020 <kj_cho@idealidea.co.kr>
2020-06-21 11:25:21 +0900

    추가 설명 주석

--- app/src/main/java/kr/co/tjoeun/apipractice_20200613/ViewReplyDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200613/ViewReplyDetailActivity.kt
@@ -16,6 +16,7 @@ class ViewReplyDetailActivity : BaseActivity() {
 
 //    답글 목록을 담고있게 될 배열
     val reReplyList = ArrayList<TopicReply>()
+//    목록을 뿌려줄 어댑터 변수
     lateinit var mReReplyAdapter : ReReplyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +33,9 @@ class ViewReplyDetailActivity : BaseActivity() {
     override fun setValues() {
         mReplyId = intent.getIntExtra("reply_id", -1)
 
+//        어댑터를 먼저 생성 => 답글 목록을 뿌려준다고 명시
         mReReplyAdapter = ReReplyAdapter(mContext, R.layout.topic_re_reply_list_item, reReplyList)
+//        어댑터와 리스트뷰 연결
         reReplyListView.adapter = mReReplyAdapter
 
 //        서버에서 의견 상세 현황 가져오기
@@ -47,7 +50,7 @@ class ViewReplyDetailActivity : BaseActivity() {
                 val reply = data.getJSONObject("reply")
                 mReply = TopicReply.getTopicReplyFromJson(reply)
 
-//                화면에 뿌려질 답글 목록도 담아주자
+//                화면에 뿌려질 답글 목록 (JSONArray)도 담아주자
                 val reReplies = reply.getJSONArray("replies")
 
                 for (i in 0..reReplies.length()-1) {
@@ -60,7 +63,7 @@ class ViewReplyDetailActivity : BaseActivity() {
                     writerNickNameTxt.text = mReply.user.nickName
                     contentTxt.text = mReply.content
 
-//                    서버에서 받아온 대댓글을 리스트뷰에 반영
+//                    서버에서 받아온 대댓글을 리스트뷰에 반영 => 리스트뷰의 내용 변경 감지 새로고침
                     mReReplyAdapter.notifyDataSetChanged()
                 }
 
